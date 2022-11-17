0dd5f58a1cf53c652ce5a10ae3ff2e5de5b96e7a
Puma23 <80652947+Puma23@users.noreply.github.com>
2022-11-16 18:46:45 -0600

    SPA JS Avance

---
+++ STEMinistA_GTO/JS/funciones.js
@@ -0,0 +1,42 @@
+
+const inicio = document.getElementById('inicio');
+const nosotras = document.getElementById('nosotras');
+const eventos = document.getElementById('eventos');
+const cursos = document.getElementById('cursos');
+const quieroSer = document.getElementById('quieroSer');
+
+const btnInicio = document.getElementById('btnInicio');
+const btnNosotras = document.getElementById('btnNosotras');
+const btnEventos = document.getElementById('btnEventos');
+const btnCursos = document.getElementById('btnCursos');
+const btnQuieroSer = document.getElementById('btnQuieroSer');
+
+
+btnInicio.addEventListener("click", () => {
+    ocultaContenido();
+    inicio.style.display = "block";
+});
+btnNosotras.addEventListener("click", () => {
+    ocultaContenido();
+    nosotras.style.display = "block";
+});
+btnEventos.addEventListener("click", () => {
+    ocultaContenido();
+    eventos.style.display = "block";
+});
+btnCursos.addEventListener("click", () => {
+    ocultaContenido();
+    cursos.style.display = "block";
+});
+btnQuieroSer.addEventListener("click", () => {
+    ocultaContenido();
+    QuieroSer.style.display = "block";
+});
+
+function ocultaContenido(){
+    const containers = document.getElementsByClassName('secciones');
+    console.log(containers);
+    for(let i = 0; i < containers.length; i++){
+        containers[i].style.display = 'none';
+    }
+};
