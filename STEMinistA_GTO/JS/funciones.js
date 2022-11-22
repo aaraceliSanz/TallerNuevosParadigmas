53e3e5c50f5642cbbf49c887638faab33696957c
Puma23 <80652947+Puma23@users.noreply.github.com>
2022-11-21 20:21:43 -0600

    correcciones SPA

--- STEMinistA_GTO/JS/funciones.js
+++ STEMinistA_GTO/JS/funciones.js
@@ -8,7 +8,13 @@ const quieroSer = document.getElementById('quieroSer');
 const btnInicio = document.getElementById('btnInicio');
 const btnNosotras = document.getElementById('btnNosotras');
 const btnEventos = document.getElementById('btnEventos');
-const btnCursos = document.getElementById('btnCursos');
+const btnHackatones = document.getElementById('btnHackatones');
+const btnBootcamps = document.getElementById('btnBootcamps');
+const btnCiencia = document.getElementById('btnCiencia');
+const btnTecnologia = document.getElementById('btnTecnologia');
+const btnIngenieria = document.getElementById('btnIngenieria');
+const btnMatematicas = document.getElementById('btnMatematicas');
+const btnCertificaciones = document.getElementById('btnCertificaciones');
 const btnQuieroSer = document.getElementById('btnQuieroSer');
 
 
@@ -23,14 +29,47 @@ btnNosotras.addEventListener("click", () => {
 btnEventos.addEventListener("click", () => {
     ocultaContenido();
     eventos.style.display = "block";
+    pie.style.display = "block";
 });
-btnCursos.addEventListener("click", () => {
+btnHackatones.addEventListener("click", () => {
+    ocultaContenido();
+    eventos.style.display = "block";
+    pie.style.display = "block";
+});
+btnBootcamps.addEventListener("click", () => {
+    ocultaContenido();
+    eventos.style.display = "block";
+    pie.style.display = "block";
+});
+btnCiencia.addEventListener("click", () => {
     ocultaContenido();
     cursos.style.display = "block";
+    pie.style.display = "block";
+});
+btnTecnologia.addEventListener("click", () => {
+    ocultaContenido();
+    cursos.style.display = "block";
+    pie.style.display = "block";
+});
+btnIngenieria.addEventListener("click", () => {
+    ocultaContenido();
+    cursos.style.display = "block";
+    pie.style.display = "block";
+});
+btnMatematicas.addEventListener("click", () => {
+    ocultaContenido();
+    cursos.style.display = "block";
+    pie.style.display = "block";
+});
+btnCertificaciones.addEventListener("click", () => {
+    ocultaContenido();
+    cursos.style.display = "block";
+    pie.style.display = "block";
 });
 btnQuieroSer.addEventListener("click", () => {
     ocultaContenido();
     quieroSer.style.display = "block";
+    pie.style.display = "block";
 });
 
 function ocultaContenido(){
